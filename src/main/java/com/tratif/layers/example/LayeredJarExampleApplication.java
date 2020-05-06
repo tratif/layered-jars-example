060535a2d5194f9ce2ab0d5aab5807459b520f37
Mateusz Wicherski <mateusz.wicherski@tratif.com>
2020-05-06 14:07:56 +0200

    Initial commit

---
+++ src/main/java/com/tratif/layers/example/LayeredJarExampleApplication.java
@@ -0,0 +1,13 @@
+package com.tratif.layers.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayeredJarExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LayeredJarExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tratif/layers/example/LayeredJarExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tratif.layers.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LayeredJarExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
