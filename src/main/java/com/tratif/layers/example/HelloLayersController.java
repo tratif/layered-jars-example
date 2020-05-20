54bacc51ba96cb51d6b406985cca3b979493dd0f
Mateusz Wicherski <mateusz.wicherski@tratif.com>
2020-05-20 15:06:58 +0200

    Make controller accept `name` parameter, update version to 0.0.2

--- src/main/java/com/tratif/layers/example/HelloLayersController.java
+++ src/main/java/com/tratif/layers/example/HelloLayersController.java
@@ -1,13 +1,19 @@
 package com.tratif.layers.example;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import static java.util.Objects.nonNull;
 
 @RestController
 public class HelloLayersController {
 
 	@GetMapping
-	public String helloLayers() {
+	public String helloLayers(@RequestParam(value = "name", required = false) String name) {
+		if (nonNull(name)) {
+			return "Hello in the world of layered JARs, " + name + "!";
+		}
 		return "Hello Layers!";
 	}
 }
